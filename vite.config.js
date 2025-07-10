3694c0d8dec71f8678c4947f1f17cd83129b3843
FedericoAzubel <48677630@est.ort.edu.ar>
2025-07-10 08:09:01 -0300

    added github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TP7-Productos-RR-Axios-Azubel/', // 👈 IMPORTANTE: el nombre del repositorio con slashs
   plugins: [react()],
 })
